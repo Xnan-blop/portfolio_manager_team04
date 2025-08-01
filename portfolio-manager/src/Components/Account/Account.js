85916a86fafc60942d28acd4d5f690977b02bfb1
yashikakashyapp <kashyapyashika16@gmail.com>
2025-08-01 14:53:14 -0400

    Creates Account files

---
+++ portfolio-manager/src/Components/Account/Account.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import './Account.css';
+
+const Account = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Account;
