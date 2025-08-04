643341641fa69a5ffb1c12cb3b43846de53bd771
suhani-kashyap <suhanikashyap081@gmail.com>
2025-08-04 15:01:50 -0400

    Displays Graph on frontend

--- portfolio-manager/src/Components/GraphContainer/graphContainer.js
+++ portfolio-manager/src/Components/GraphContainer/graphContainer.js
@@ -1,28 +1,53 @@
 // GraphContainer.jsx
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Chart } from "chart.js/auto";
 import './graphContainer.css';
 
 const GraphContainer = () => {
+  const [portfolioData, setPortfolioData] = useState([]);
+  
+  useEffect(() => {
+    const PortfolioData = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5050/api/portfolio/value');
+      const data = await response.json();
+      setPortfolioData(data);
+    } catch (err) {
+      console.error('Failure fetching portfolio data for graph');
+    }
+  };
+  PortfolioData();
+  }, []);
+
   const chartRef = useRef(null);
   const chartInstanceRef = useRef(null); // to prevent duplicate chart renders
 
   useEffect(() => {
+    if (portfolioData.length === 0) return;
+
     const ctx = chartRef.current;
 
     if (chartInstanceRef.current) {
       chartInstanceRef.current.destroy();
     }
 
+    console.log(portfolioData);
+    const x_axis = portfolioData.map(item => item.date);
+    const y_axis = portfolioData.map(item => item.total_value);
+    console.log(x_axis);
+    console.log(y_axis);
+    console.log(Math.max(y_axis));
+
     chartInstanceRef.current = new Chart(ctx, {
       type: "line",
       data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
+        labels: x_axis,
         datasets: [{
-          data: [7, 8, 8, 9, 4, 9, 10, null],
+          data: y_axis,
           fill: false,
-          borderColor: "rgba(0,0,255,1.0)",
-          tension: 0.1
+          borderColor: "#fff",
+          tension: 0.1,
+          pointRadius: 0
         }]
       },
       options: {
@@ -31,11 +56,23 @@ const GraphContainer = () => {
           legend: { display: false }
         },
         scales: {
-          y: {
-            min: 0,
-            max: 15,
+          x: {
             ticks: {
-              stepSize: 1
+              color: "#fff",
+            },
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            grid: {
+              display: false
+            },
+            min: Math.min(...y_axis) - 50,
+            max: Math.max(...y_axis) + 50,
+            ticks: {
+              stepSize: 1,
+              color: "#fff"
             }
           }
         }
@@ -45,7 +82,9 @@ const GraphContainer = () => {
     return () => {
       chartInstanceRef.current?.destroy();
     };
-  }, []);
+  }, [portfolioData]);
+
+  
 
   return (
     <div className="graph-container dark-mode">
